af94ac8731ae37d0f1a16b47364d46a41832d094
sam6321 <sam6321@live.com.au>
2019-04-27 20:01:03 +0800

    Working on basic path asset

---
+++ Assets/Scripts/Pathing/PathNode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathNodeEntry
+{
+    private PathNode node;
+    private bool enabled;
+
+    public PathNodeEntry(PathNode node, bool enabled)
+    {
+        this.node = node;
+        this.enabled = enabled;
+    }
+
+    public PathNode Node
+    {
+        get { return node; }
+    }
+
+    public bool Enabled
+    {
+        get { return enabled; }
+        set { enabled = value; }
+    }
+}
+
+[Serializable]
+public class PathNode
+{
+    private static long nextId = 0;
+
+    private long id = nextId++;
+    private List<PathNodeEntry> links = new List<PathNodeEntry>();
+    public Vector3 Position { get; set; }
+    public IReadOnlyList<PathNodeEntry> Links { get { return links; } }
+
+    public void SetLink(PathNode next, bool enabled=true)
+    {
+        PathNodeEntry link = GetLink(next);
+        if(link == null)
+        {
+            link = new PathNodeEntry(next, enabled);
+            links.Add(link);
+        }
+        else
+        {
+            link.Enabled = enabled;
+        }
+    }
+
+    public bool RemoveLink(PathNode next)
+    {
+        int index = links.FindIndex(entry => entry.Node == next);
+        if(index >= 0)
+        {
+            links.RemoveAt(index);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool HasLink(PathNode next)
+    {
+        return GetLink(next) != null;
+    }
+
+    private PathNodeEntry GetLink(PathNode next)
+    {
+        return links.Find(entry => entry.Node == next);
+    }
+}
