7143f1f4595350ff89a20ac36e19b62b30224732
sam6321 <sam6321@live.com.au>
2019-04-28 20:22:09 +0800

    Added basic junction to swap between two paths

---
+++ Assets/Scripts/Junction.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Junction : MonoBehaviour
+{
+    [SerializeField]
+    private PathNode node;
+
+    private PathNode[] nextNodes;
+    private int index = 0;
+
+    void Start()
+    {
+        nextNodes = node.Next.ToArray();
+        if(nextNodes.Length > 0)
+        {
+            foreach (PathNode next in nextNodes)
+            {
+                next.NodeEnabled = false;
+            }
+
+            nextNodes[0].NodeEnabled = true;
+        }
+    }
+
+    void OnClick()
+    {
+        Debug.Log("On click");
+
+        nextNodes[index].NodeEnabled = false;
+        index = (index + 1) % nextNodes.Length;
+        nextNodes[index].NodeEnabled = true;
+    }
+}
---
+++ Assets/Scripts/MousePickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MousePickup : MonoBehaviour
+{
+    int clickableMask = 0;
+
+    void Start()
+    {
+        clickableMask = LayerMask.GetMask("MouseInteractable");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, clickableMask))
+            {
+                hit.collider.gameObject.SendMessage("OnClick");
+            }
+        }
+    }
+
+}
