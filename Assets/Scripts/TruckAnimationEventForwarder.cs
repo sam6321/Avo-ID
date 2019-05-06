e815f2d1c19c1da7baa5d9dad6ef0587e39a58b3
sam6321 <sam6321@live.com.au>
2019-05-06 17:54:35 +0800

    Added truck drive away / return sounds

--- Assets/Scripts/TruckAnimationEventForwarder.cs
+++ Assets/Scripts/TruckAnimationEventForwarder.cs
@@ -7,9 +7,32 @@ public class TruckAnimationEventForwarder : MonoBehaviour
     [SerializeField]
     private GameLogic logic;
 
+    [SerializeField]
+    private AudioClip driveAwaySound;
+
+    [SerializeField]
+    private AudioClip returnSound;
+
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void OnDriveAwayBegin()
+    {
+        source.PlayOneShot(driveAwaySound);
+    }
+
     public void OnDriveAwayComplete()
     {
         logic.OnTruckDriveAwayComplete();
+    }
+
+    public void OnReturnBegin()
+    {
+        source.PlayOneShot(returnSound);
     }
 
     public void OnReturnComplete()
